313fe7283a87af50cb5806a0d1f4a687dd004868
Johnny Domino <jvdomino@gmail.com>
2023-02-28 20:25:59 -0500

    pass params via query string rather than body
    and prefix with `https://`

--- __tests__/ezql.ts
+++ __tests__/ezql.ts
@@ -87,8 +87,7 @@ describe('register EZQL', () => {
     const phrase = 'What is the answer to life, the univerise and everything?'
     const response = await ob.prompt(phrase, Prompt.data)
 
-    expect(fetch).toHaveBeenCalledWith(`${DEFAULT_HOST}/ezql`, {
-      body: JSON.stringify({ phrase, type: 'data' }),
+    expect(fetch).toHaveBeenCalledWith(`https://${DEFAULT_HOST}/ezql?phrase=What+is+the+answer+to+life%2C+the+univerise+and+everything%3F&type=data`, {
       headers: { Authorization: `Bearer ${token}` },
     })
     expect(response).toEqual(mockedResponse)
@@ -103,8 +102,7 @@ describe('register EZQL', () => {
     const phrase = 'What is the answer to life, the univerise and everything?'
     const response = await ob.prompt(phrase, Prompt.sql)
 
-    expect(fetch).toHaveBeenCalledWith(`${DEFAULT_HOST}/ezql`, {
-      body: JSON.stringify({ phrase, type: 'sql' }),
+    expect(fetch).toHaveBeenCalledWith(`https://${DEFAULT_HOST}/ezql?phrase=What+is+the+answer+to+life%2C+the+univerise+and+everything%3F&type=sql`, {
       headers: { Authorization: `Bearer ${token}` },
     })
     expect(response).toEqual(mockedResponse)
