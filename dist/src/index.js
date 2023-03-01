d6590dbb3180997b66f19da3818a317d85306a76
Johnny Domino <jvdomino@gmail.com>
2023-02-28 20:28:01 -0500

    FRESH BOTS!
    builds + pots => bots

---
+++ dist/src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.EZQL = exports.Prompt = void 0;
+const ezql_1 = require("./ezql");
+Object.defineProperty(exports, "Prompt", { enumerable: true, get: function () { return ezql_1.Prompt; } });
+Object.defineProperty(exports, "EZQL", { enumerable: true, get: function () { return ezql_1.EZQL; } });
+//# sourceMappingURL=index.js.map
