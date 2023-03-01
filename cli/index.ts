4ee6c8a08a83664b1a48ec4bd84535c82d981dfc
Johnny Domino <jvdomino@gmail.com>
2023-02-28 20:27:40 -0500

    add CLI

---
+++ cli/index.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+
+import { Command } from '@commander-js/extra-typings'
+import { EZQL, Prompt } from '../src/index'
+import { version } from '../package.json'
+
+const program = new Command()
+
+program
+  .version(version)
+  .description('A natural language to SQL converter')
+  .argument('<phrase>', 'A query written in natural language to be executed against your database.')
+  .option('-e, --execute', 'Specifies whether to automatically execute the generated SQL statement. Defaults to false.')
+  .option(
+    '-t, --token <value>',
+    'Identifies which database to run queries against. This is provided by Outerbase. May also be provided via the environment variable "OUTERBASE_EZQL_TOKEN"'
+  )
+  .option('-h, --host <value>', 'API host used for requests. Defaults to "api.outerbase.com".')
+  .action(async (query, { execute, host, token: _token }) => {
+    const token = _token || process.env.OUTERBASE_EZQL_TOKEN
+    if (!token) throw new Error('"token" must be provided either as an argument (--token) or via the environment variable "OUTERBASE_EZQL_TOKEN"')
+
+    const ezql = new EZQL({ token, host })
+    const response = await ezql.prompt(query, execute ? Prompt.data : Prompt.sql)
+
+    // TODO fancy formatting and/or syntax highlighting
+    console.log(response)
+  })
+  .parse(process.argv)
