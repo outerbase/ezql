f081e4f065372d2c63f27a62e17deaee4a811d42
Johnny Domino <jvdomino@gmail.com>
2023-03-02 18:52:23 -0500

    add minimized version of UMD build

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,12 +1,15 @@
 import typescript from '@rollup/plugin-typescript'
+import terser from '@rollup/plugin-terser'
+
+const outputOpts = {
+  name: 'Outerbase',
+  file: 'dist/umd/bundle.js',
+  format: 'umd',
+  sourcemap: true,
+}
 
 export default {
   input: 'src/lib/index.ts',
-  output: {
-    name: 'Outerbase',
-    file: 'dist/umd/bundle.js',
-    format: 'umd',
-    sourcemap: true,
-  },
+  output: [outputOpts, { ...outputOpts, file: 'dist/umd/bundle.min.js', sourcemap: false, plugins: [terser()] }],
   plugins: [typescript({ tsconfig: 'config/tsconfig.umd.json' })],
 }
